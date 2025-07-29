5dbf18d7629f4cf5ee0fdaf9250c37a684b97f8f
Manuel A. Rodríguez Moreno <125369826+ManoloRm@users.noreply.github.com>
2025-07-29 12:12:32 -0600

    Update pages.py

--- pages.py
+++ pages.py
@@ -4,28 +4,32 @@ import helpers
 from selenium.webdriver.common.by import By
 
 class UrbanRoutesPage:
-    from_field = (By.ID, 'from')
     to_field = (By.ID, 'to')
     button_round = (By.ID, 'button round')
     payment_button = (By.ID, "pp-button filled")
-    confort_button = (By.ID)
-    phonh_num = (By.CLASS_NAME, 'np-button')
+    phon_num = (By.CLASS_NAME, 'np-button')
+    car_comfort = (By.CSS_SELECTOR,'#root > div > div.workflow > div.workflow-subcontainer > div.tariff-picker.shown > div.tariff-cards > div.tcard.active > button')
 
 
     def __init__(self, driver):
         self.driver = driver
 
+    def get_button_round(self):
+        return self.driver.find_element(By.CSS_SELECTOR, '#root > div > div.workflow > div.workflow-subcontainer > div.type-picker.shown > div.results-container > div.results-text > button').click()
+
+    #def get_payment_button(self):
+
     def set_from(self, from_address):
-        self.driver.find_element(*self.from_field).send_keys(from_address)
+        self.driver.find_element(By.ID, 'from').send_keys(from_address)
 
     def set_to(self, to_address):
-        self.driver.find_element(*self.to_field).send_keys(to_address)
+        self.driver.find_element(By.ID, 'to').send_keys(to_address)
 
     def get_from(self):
-        return self.driver.find_element(*self.from_field).get_property('value')
+        return self.driver.find_elemen(By.ID, 'from').get_property('value')
 
     def get_to(self):
-        return self.driver.find_element(*self.to_field).get_property('value')
+        return self.driver.find_element(By.ID, 'to').get_property('value')
 
     def select_rout(self):
         self.driver.find_element(By.CSS_SELECTOR, '#root > div > div.workflow > div.workflow-subcontainer > div.type-picker.shown > div.modes-container > div.mode.active').click()
@@ -39,3 +43,13 @@ class UrbanRoutesPage:
 
     def set_card_number(self):
         self.driver.find_element(*self.button_round).click()
+
+    def get_confort(self):
+        return self.driver.find_elements(By.CSS_SELECTOR, '#root > div > div.workflow > div.workflow-subcontainer > div.type-picker.shown > div.results-container > div.results-text > button')
+
+    def get_confort_nt(self):
+        return  self.driver.find_elements(By.CLASS_NAME, 'tcard-title')
+
+    def set_confort(self):
+        return self.driver.find_element(By.CSS_SELECTOR, '#root > div > div.workflow > div.workflow-subcontainer > div.tariff-picker.shown > div.tariff-cards > div.tcard.active > button')
+
