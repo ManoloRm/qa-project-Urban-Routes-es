a46cb8461df616319a58e4a9cbe13cf7557c101e
Manuel A. Rodríguez Moreno <125369826+ManoloRm@users.noreply.github.com>
2025-07-29 12:12:11 -0600

    Update main.py

--- main.py
+++ main.py
@@ -33,32 +33,28 @@ class TestUrbanRoutes:
         self.driver.get(data.urban_routes_url)
         test_address_form = data.address_from
         test_address_to = data.address_to
-        self.driver.find_element(By.XPATH, '//*[@id="from"]').send_keys(test_address_form)
-        self.driver.find_element(By.XPATH, '//*[@id="to"]').send_keys(test_address_to)
+        pages.UrbanRoutesPage.set_from(self, test_address_form)
+        pages.UrbanRoutesPage.set_to(self, test_address_to)
         assert test_address_form == data.address_from
         assert test_address_to == data.address_to
 
-    def test_select_confort(self):
+    def test_select_comfort(self):
         self.driver.get(data.urban_routes_url)
         test_address_form = data.address_from
         test_address_to = data.address_to
-        self.driver.find_element(By.XPATH, '//*[@id="from"]').send_keys(test_address_form)
-        self.driver.find_element(By.XPATH, '//*[@id="to"]').send_keys(test_address_to)
-        assert test_address_form == data.address_from
-        assert test_address_to == data.address_to
-        self.driver.find_element(By.CSS_SELECTOR,'#root > div > div.workflow > div.workflow-subcontainer > div.type-picker.shown > div.results-container > div.results-text > button').click()
-        card_title_elements = self.driver.find_elements(By.CLASS_NAME, 'tcard-title')
-        car_comfort = card_title_elements[4]
+        pages.UrbanRoutesPage.set_from(self, test_address_form)
+        pages.UrbanRoutesPage.set_to(self, test_address_to)
+        pages.UrbanRoutesPage.get_button_round(self)
+        car_comfort = pages.UrbanRoutesPage.set_confort(self)
         assert car_comfort.is_enabled()
         car_comfort.click()
-        wait = WebDriverWait(self.driver, 2)
 
     def test_phone_number(self):
         self.driver.get(data.urban_routes_url)
         test_address_form = data.address_from
         test_address_to = data.address_to
-        self.driver.find_element(By.XPATH, '//*[@id="from"]').send_keys(test_address_form)
-        self.driver.find_element(By.XPATH, '//*[@id="to"]').send_keys(test_address_to)
+        pages.UrbanRoutesPage.set_from(self, test_address_form)
+        pages.UrbanRoutesPage.set_to(self, test_address_to)
         assert test_address_form == data.address_from
         assert test_address_to == data.address_to
         self.driver.find_element(By.CSS_SELECTOR, '#root > div > div.workflow > div.workflow-subcontainer > div.type-picker.shown > div.results-container > div.results-text > button').click()
@@ -71,25 +67,22 @@ class TestUrbanRoutes:
         WebDriverWait(self.driver, 2)
         phone_number = data.phone_number
         assert phone_number == data.phone_number
-        phone_text_select = self.driver.find_element(By.CSS_SELECTOR,'#root > div > div.number-picker.open > div.modal > div.section.active > form > div.np-input > div.input-container')
+        phone_text_select = self.driver.find_element(By.ID, 'phone')
         assert (EC.visibility_of_element_located(phone_text_select))
         phone_text_select.send_keys(phone_number)
-        WebDriverWait(self.driver, 2)
-        phone_code = retrieve_phone_code(driver=self.driver)
-        phone_code_code_text = self.driver.find_element(By.CSS_SELECTOR, '#root > div > div.number-picker.open > div.modal > div.section.active > form > div.np-input > div')
-        phone_code_code_text.send_keys(phone_number)
-        assert phone_code == retrieve_phone_code(driver=self.driver)
-        WebDriverWait(self.driver, 2)
-        self.driver.find_element(By.CSS_SELECTOR, '#root > div > div.number-picker.open > div.modal > div.section.active > form > div.np-input > div').send_keys(phone_code)
-        self.driver.find_element(By.CSS_SELECTOR, '#root > div > div.number-picker.open > div.modal > div.section.active > form > div.buttons > button:nth-child(1)').click()
         self.driver.find_element(By.CSS_SELECTOR, '#root > div > div.number-picker.open > div.modal > div.section.active > form > div.buttons > button').click()
+        phone_code = retrieve_phone_code(self.driver)
+        phone_code_code_text = self.driver.find_element(By.CSS_SELECTOR, '#code')
+        phone_code_code_text.send_keys(phone_code)
+        self.driver.find_element(By.CSS_SELECTOR, '#root > div > div.number-picker.open > div.modal > div.section.active > form > div.buttons > button:nth-child(1)').click()
+
 
     def test_add_new_card(self):
         self.driver.get(data.urban_routes_url)
         test_address_form = data.address_from
         test_address_to = data.address_to
-        self.driver.find_element(By.XPATH, '//*[@id="from"]').send_keys(test_address_form)
-        self.driver.find_element(By.XPATH, '//*[@id="to"]').send_keys(test_address_to)
+        pages.UrbanRoutesPage.set_from(self, test_address_form)
+        pages.UrbanRoutesPage.set_to(self,test_address_to)
         assert test_address_form == data.address_from
         assert test_address_to == data.address_to
         self.driver.find_element(By.CSS_SELECTOR, '#root > div > div.workflow > div.workflow-subcontainer > div.type-picker.shown > div.results-container > div.results-text > button').click()
@@ -97,25 +90,18 @@ class TestUrbanRoutes:
         car_comfort = card_title_elements[4]
         assert car_comfort.is_enabled()
         car_comfort.click()
-        WebDriverWait(self.driver, 20)
         self.driver.find_element(By.CSS_SELECTOR, '#root > div > div.workflow > div.workflow-subcontainer > div.tariff-picker.shown > div.form > div.pp-button.filled').click()
-        WebDriverWait(self.driver, 100)
         self.driver.find_element(By.CSS_SELECTOR, '#root > div > div.payment-picker.open > div.modal > div.section.active > div.pp-selector > div.pp-row.disabled').click()
-        WebDriverWait(self.driver, 20)
         self.driver.find_element(By.CSS_SELECTOR, '#number').send_keys(data.card_number)
-        WebDriverWait(self.driver, 20)
-        code_card_elements = self.driver.find_elements(By.CLASS_NAME,'code')
-        code_card = code_card_elements[3]
-        WebDriverWait(self.driver, 20)
+        code_card = self.driver.find_element(By.NAME,'code')
         code_card.send_keys(data.card_code)
 
     def test_message_driver(self):
         self.driver.get(data.urban_routes_url)
-        message_test = data.message_for_driver
         test_address_form = data.address_from
         test_address_to = data.address_to
-        self.driver.find_element(By.XPATH, '//*[@id="from"]').send_keys(test_address_form)
-        self.driver.find_element(By.XPATH, '//*[@id="to"]').send_keys(test_address_to)
+        pages.UrbanRoutesPage.set_from(self, test_address_form)
+        pages.UrbanRoutesPage.set_to(self,test_address_to)
         assert test_address_form == data.address_from
         assert test_address_to == data.address_to
         self.driver.find_element(By.CSS_SELECTOR, '#root > div > div.workflow > div.workflow-subcontainer > div.type-picker.shown > div.results-container > div.results-text > button').click()
@@ -123,18 +109,16 @@ class TestUrbanRoutes:
         car_comfort = card_title_elements[4]
         assert car_comfort.is_enabled()
         car_comfort.click()
-        WebDriverWait(self.driver, 20)
-        message_box = self.driver.find_element(By.XPATH, '//*[@id="root"]/div/div[3]/div[3]/div[2]/div[2]/div[3]/div')
-        message_box.click()
-        message_box.send_keys(message_test)
+        message_box = self.driver.find_element(By.CSS_SELECTOR, '#comment')
+        message_box.send_keys(data.card_number)
         WebDriverWait(self.driver, 20)
 
     def test_manta_panuelos(self):
         self.driver.get(data.urban_routes_url)
         test_address_form = data.address_from
         test_address_to = data.address_to
-        self.driver.find_element(By.XPATH, '//*[@id="from"]').send_keys(test_address_form)
-        self.driver.find_element(By.XPATH, '//*[@id="to"]').send_keys(test_address_to)
+        pages.UrbanRoutesPage.set_from(self, test_address_form)
+        pages.UrbanRoutesPage.set_to(self,test_address_to)
         assert test_address_form == data.address_from
         assert test_address_to == data.address_to
         self.driver.find_element(By.CSS_SELECTOR, '#root > div > div.workflow > div.workflow-subcontainer > div.type-picker.shown > div.results-container > div.results-text > button').click()
@@ -142,7 +126,6 @@ class TestUrbanRoutes:
         car_comfort = card_title_elements[4]
         assert car_comfort.is_enabled()
         car_comfort.click()
-        WebDriverWait(self.driver, 20)
         self.driver.find_element(By.CSS_SELECTOR, '#root > div > div.workflow > div.workflow-subcontainer > div.tariff-picker.shown > div.form > div.reqs.open > div.reqs-body '
                                                   '> div:nth-child(1) > div > div.r-sw > div > span').click()
 
@@ -150,8 +133,8 @@ class TestUrbanRoutes:
         self.driver.get(data.urban_routes_url)
         test_address_form = data.address_from
         test_address_to = data.address_to
-        self.driver.find_element(By.XPATH, '//*[@id="from"]').send_keys(test_address_form)
-        self.driver.find_element(By.XPATH, '//*[@id="to"]').send_keys(test_address_to)
+        pages.UrbanRoutesPage.set_from(self, test_address_form)
+        pages.UrbanRoutesPage.set_to(self,test_address_to)
         assert test_address_form == data.address_from
         assert test_address_to == data.address_to
         self.driver.find_element(By.CSS_SELECTOR, '#root > div > div.workflow > div.workflow-subcontainer > div.type-picker.shown > div.results-container > div.results-text > button').click()
@@ -159,10 +142,6 @@ class TestUrbanRoutes:
         car_comfort = card_title_elements[4]
         assert car_comfort.is_enabled()
         car_comfort.click()
-        WebDriverWait(self.driver, 20)
-        helado_count = self.driver.find_element(By.CSS_SELECTOR,
-                                                '#root > div > div.workflow > div.workflow-subcontainer > div.tariff-picker.shown > div.form > div.reqs.open > div.reqs-body '
-                                                '> div.r.r-type-group > div > div.r-group-items > div:nth-child(1) > div > div.r-counter > div > div.counter-value')
         helados = self.driver.find_element(By.CSS_SELECTOR,
                                            '#root > div > div.workflow > div.workflow-subcontainer > div.tariff-picker.shown > div.form > div.reqs.open > div.reqs-body '
                                            '> div.r.r-type-group > div > div.r-group-items > div:nth-child(1) > div > div.r-counter > div > div.counter-plus')
@@ -173,8 +152,8 @@ class TestUrbanRoutes:
         self.driver.get(data.urban_routes_url)
         test_address_form = data.address_from
         test_address_to = data.address_to
-        self.driver.find_element(By.XPATH, '//*[@id="from"]').send_keys(test_address_form)
-        self.driver.find_element(By.XPATH, '//*[@id="to"]').send_keys(test_address_to)
+        pages.UrbanRoutesPage.set_from(self, test_address_form)
+        pages.UrbanRoutesPage.set_to(self,test_address_to)
         assert test_address_form == data.address_from
         assert test_address_to == data.address_to
         self.driver.find_element(By.CSS_SELECTOR, '#root > div > div.workflow > div.workflow-subcontainer > div.type-picker.shown > div.results-container > div.results-text > button').click()
